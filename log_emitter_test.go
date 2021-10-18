aa60a01995116c323e796fa07d2b1f03051c95b3
Oscar <fu.wire@gmail.com>
2021-10-18 12:19:59 -0500

    tests(log): add tests

--- log_emitter_test.go
+++ log_emitter_test.go
@@ -2,40 +2,45 @@ package shared
 
 import (
 	"bytes"
+	"fmt"
 	. "github.com/onsi/gomega"
 	"github.com/paketo-buildpacks/packit"
 	"github.com/sclevine/spec"
-	"github.com/sclevine/spec/report"
 	"testing"
 )
 
-func testLogEmitter(t *testing.T, context spec.G, it spec.S) {
-
-	var (
-		Expect = NewWithT(t).Expect
-
-		buffer  *bytes.Buffer
-		emitter LogEmitter
-	)
-
-	it.Before(func() {
-		buffer = bytes.NewBuffer(nil)
-		emitter = NewLogEmitter(buffer)
-	})
-
-	context("Environment", func() {
-		it("prints details about the environment", func() {
-			emitter.Environment(packit.Environment{
-				"GEM_PATH.override": "/some/path",
+func TestLogEmitter(
+	t *testing.T,
+) {
+	t.Helper()
+	RegisterFailHandler(fail)
+	spec.Run(
+		t,
+		"Run",
+		func(
+			t *testing.T,
+			when spec.G,
+			it spec.S) {
+			var (
+				buffer  *bytes.Buffer
+				emitter LogEmitter
+			)
+			it.Before(func() {
+				buffer = bytes.NewBuffer(nil)
+				emitter = NewLogEmitter(buffer)
 			})
 
-			Expect(buffer.String()).To(ContainSubstring("    GEM_PATH -> \"/some/path\""))
-		})
-	})
+			it("prints details about the environment", func() {
+				emitter.Environment(packit.Environment{
+					"GEM_PATH.override": "/some/path",
+				})
+				Expect(buffer.String()).To(ContainSubstring("    GEM_PATH -> \"/some/path\""))
+			})
+		},
+	)
 }
 
-func TestUnitMRI(t *testing.T) {
-	suite := spec.New("shared", spec.Report(report.Terminal{}))
-	suite("LogEmitter", testLogEmitter)
-	suite.Run(t)
+func fail(message string, skip ...int) {
+	fmt.Println(message)
+	fmt.Println(skip)
 }
